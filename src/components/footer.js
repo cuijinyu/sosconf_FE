036376366690cdc7861abd97ab3b15041b1cab70
cuijinyu <c2510271615@gmail.com>
2019-03-05 14:36:15 +0800

    完成了首页的布局,但是还是不够美观

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,16 +5,18 @@ class Footer extends Component {
   render() {
     return (
       <div className="sosconf-footer__wrap">
-        <div className="sosconf-footer__left-logo">
+        <div className="sosconf-footer__logo-wrap">
+            <div className="sosconf-footer__left-logo">
+            </div>
         </div>
         <div className="sosconf-footer__middle-buttons">
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
-            <a></a>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-button--openingsource"></button>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-mail"></button>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-facebook"></button>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-twitter"> </button>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-flickr"></button>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-youtube"></button>
+            <button className="sosconf-footer__middle-button sosconf-footer__middle-weibo"></button>
         </div>
         <div className="sosconf-footer__copyleft">
           <p>Copyleft @ openingsource.org</p>
