a7e652ef6a1efc76c914f2577efd029dd26d661d
cuijinyu <c2510271615@gmail.com>
2019-03-11 13:39:13 +0800

    Update:新增响应式

--- src/components/header.js
+++ src/components/header.js
@@ -5,35 +5,44 @@ class Header extends Component {
   render() {
     return (
       <div className="sosconf-header__wrap">
-        <div className="sosconf-header__logo">
-          <div className="sosconf-header__logo--svg"></div>
+        <div className="sosconf-header-pc">
+          <div className="sosconf-header__logo">
+            <div className="sosconf-header__logo--svg"></div>
+            <div className="sosconf-header__logo--name">sosconf</div>
+            <div className="sosconf-header__logo--name-number">19</div>
+          </div>
+          <div className="sosconf-header__buttons">
+            <button className="sosconf-header__button">首页</button>
+            <div className="sosconf-header__button-group sosconf-header__button-group--about">             
+              <button className="sosconf-header__group-button-title" id="sosconf-header__button--about">关于</button>
+              <div className="sosconf-header__gourp-drop-down">
+                <button className="sosconf-header__group-button">关于年会</button>
+                <button className="sosconf-header__group-button">工作人员名单</button>
+              </div>
+            </div>
+            <button className="sosconf-header__button">赞助</button>
+            {/* 无 */}
+            <button className="sosconf-header__button">议程</button>
+            <button className="sosconf-header__button">导航</button>
+            <div className="sosconf-header__button-group sosconf-header__button-group--about">             
+              <button className="sosconf-header__group-button-title" id="sosconf-header__button--contact">联系</button>
+              <div className="sosconf-header__gourp-drop-down">
+                <button className="sosconf-header__group-button">赞助联系</button>
+                <button className="sosconf-header__group-button">讲者联系</button>
+                <button className="sosconf-header__group-button">志愿者联系</button>
+              </div>
+            </div>
+          </div>
+          <div className="sosconf-header__choose">
+            
+          </div>
+        </div>
+
+        <div className="sosconf-header-mobile">
+          <div className="sosconf-header-mobile__logo">
+          </div>
           <div className="sosconf-header__logo--name">sosconf</div>
           <div className="sosconf-header__logo--name-number">19</div>
-        </div>
-        <div className="sosconf-header__buttons">
-          <button className="sosconf-header__button">首页</button>
-          <div className="sosconf-header__button-group sosconf-header__button-group--about">             
-            <button className="sosconf-header__group-button-title" id="sosconf-header__button--about">关于</button>
-            <div className="sosconf-header__gourp-drop-down">
-              <button className="sosconf-header__group-button">关于年会</button>
-              <button className="sosconf-header__group-button">工作人员名单</button>
-            </div>
-          </div>
-          <button className="sosconf-header__button">赞助</button>
-          {/* 无 */}
-          <button className="sosconf-header__button">议程</button>
-          <button className="sosconf-header__button">导航</button>
-          <div className="sosconf-header__button-group sosconf-header__button-group--about">             
-            <button className="sosconf-header__group-button-title" id="sosconf-header__button--contact">联系</button>
-            <div className="sosconf-header__gourp-drop-down">
-            <button className="sosconf-header__group-button">赞助联系</button>
-            <button className="sosconf-header__group-button">讲者联系</button>
-            <button className="sosconf-header__group-button">志愿者联系</button>
-            </div>
-          </div>
-        </div>
-        <div className="sosconf-header__choose">
-          
         </div>
       </div>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -22,12 +22,27 @@ class Home extends Component {
                 关于年会
               </button>
             </div>
+              <div className="sosconf-home__news">
+                  <div className="sosconf-home__news--container">
+                      <div className="sosconf-home__news--head">最近新闻</div>
+                      <div className="sosconf-home__news--more">更多新闻-></div>
+                      <div className="sosconf-home__news--block"></div>
+                      <h1 className="sosconf-home__news--title">第一届全球学生开源年会 (sosconf) 即将举办</h1>
+                      <div className="sosconf-home__news--span"></div>
+                      <div className="sosconf-home__news--summary">Students Open Source Conference (简称 sosconf), 首个由学生组织面向学生的非盈利社区全球性开源技术峰会。将于 2019年8月下旬在美国南加州大学举办</div>
+
+                      <div className="sosconf-home__news--image"></div>
+                      <div className="sosconf-home__news--hr"></div>
+                      <div className="sosconf-home__news--content">
+                          <p> sosconf的前身来自于2018年在重庆举办的第0届学生开源年会 (SOSCON)，从2019年第1届学生开源年会开始，sosconf成为全球学生开源会议，并计划在不同国家举办当地语言版本的学生开源年会。 峰会基于开放源代码的理念，鼓励学生享受开源、了解开源、参与开源、贡献开源，并能从开源中取得实践和乐趣。</p>
+                          <p>全球学生开源年会（sosconf）致力于纯粹的技术交流和多元的思想碰撞，为学生群体建立活跃、多元、纯粹、关注学习和未来的技术社区。该会议旨在积极推动学生参与开放源代码的历史进程。因此，本次年会以将围绕着“开源”进行，从今年来作为当红炸子鸡的机器学习到用途广泛的 UI 设计，再到时下热门的
+                          区块链领域。凡此种种，无一不成为参会人的技术盛宴。在来自各个高校的学生讲者和各领域的专业人士的参与下，年会将成为一个多元和友好的国际交流平台，我们鼓励学生亲临现场，交流在开源社区积累的点滴经验，分享蕴含在编码中的激情，畅聊属于开源理念的未来。</p>
+                          <p>2019年，在南加州大学开源协会（SOSSC) 和中国高校的开源组织共同努力之下，sosconf 2019，即第1届全球学生开源年会将于 2019 年 8 月下旬在美国南加州大学举办，如果你对本次峰会感兴趣，请关注官方网站 http://sosconf.org 获取最新最详细的信息。</p>
+                      </div>
+                  </div>
+              </div>
           </div>
-          <div className="sosconf-home__news">
-            <div>
-              
-            </div>
-          </div>
+
         </div>
 
       </div>
